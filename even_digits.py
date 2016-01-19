373c7eca310c448c73eab8e6568c453124d73220
Jack <jack.grahl@yahoo.co.uk>
2016-01-19 23:22:50 +0000

    Add function is_microwave.
    Integers are in our sequence if this function returns true.

--- even_digits.py
+++ even_digits.py
@@ -6,3 +6,6 @@ def counts(n):
     s = "".join(str(i) for i in l)
     return [ s.count(str(j)) for j in range(10) ]
 
+def is_microwave(n):
+    c = counts(n)
+    return min(c) >= max(c) - 1
--- tests/test_even_digits.py
+++ tests/test_even_digits.py
@@ -1,9 +1,13 @@
 import unittest
 
-from even_digits import counts
+from even_digits import counts, is_microwave
 
 class TestEvenDigits(unittest.TestCase):
     def test_counts_for_2(self):
         """ 2, 4, 6, 8, 10, 12, 14, 16, 18 """
         expected = [1, 5, 2, 0, 2, 0, 2, 0, 2, 0]
         self.assertEqual(counts(2), expected)
+
+    def test_is_microwave_obvious_values(self):
+        self.assertTrue(is_microwave(1))
+        self.assertFalse(is_microwave(2))
